0cfa6a5df5ab5c83c8a12e9a5a47cbc791b57a09
zidol <ajh3166@naver.com>
2021-06-26 22:22:33 +0900

    RestTemplate Post

--- src/main/java/com/example/client/service/RestTemplateService.java
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -39,5 +39,30 @@ public class RestTemplateService {
         return result.getBody();
     }
 
+    public void post() {
+        // http://localhost:9090/api/server/user/{userId}/name/{userName}
 
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println("uri = " + uri);
+
+        // http body -> Object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+        System.out.println("response.getStatusCode() = " + response.getStatusCode());
+        System.out.println("response.getHeaders() = " + response.getHeaders());
+        System.out.println("response.getBody() = " + response.getBody());
+
+//        return response.getBody();
+    }
 }
